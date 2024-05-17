bfac4c0eb9de5058a94194b84c2e6bd5265cc57e
Roboman01851 <roboman01851@gmail.com>
2024-05-17 17:34:08 -0400

    commit

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-from math import *
+import pygame, math, settings
 
-import pygame
-from pygame import *
 
 pygame.init()
-game_window = pygame.display.set_mode((width, height))
-pygame.display.set_caption(f"{title}")
+game_window = pygame.display.set_mode((settings.width, settings.height))
+pygame.display.set_caption(f"{settings.title}")
 clock = pygame.time.Clock()
-running = True
 
 def startProgram():
+    running = True
+
+    center_x, center_y = settings.width // 2, settings.height // 2
+    radius = 100
+    vertex_count = 5
 
     while running:
 
         for event in pygame.event.get():
-            print(f"{event}")
             if event.type == pygame.QUIT:
                 running = False
 
+        game_window.fill((0,0,0))
+
+        vertices = [
+            (
+                center_x + radius * math.cos(2 * math.pi * i / vertex_count),
+                center_y + radius * math.sin(2 * math.pi * i / vertex_count),
+            )
+            for i in range(vertex_count)
+        ]
+
+        pygame.draw.polygon(game_window, (255, 255, 255), vertices)
+
+        button_pressed = pygame.key.get_pressed()
+        if vertex_count >= 4:
+            if button_pressed[pygame.K_UP]:
+                vertex_count += 1
+            if button_pressed[pygame.K_DOWN]:
+                vertex_count -= 1
+        print(vertex_count)
+
+        pygame.display.flip()
 
     pygame.quit()
 
